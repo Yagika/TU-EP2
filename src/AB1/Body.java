dfbbf5f065652aa3db9701c063417bade78e1f19
Yagika <kucherenkoyanna@gmail.com>
2024-03-18 08:07:45 +0100

    Abgabe

--- src/AB1/Body.java
+++ src/AB1/Body.java
@@ -8,21 +8,27 @@ import codedraw.CodeDraw;
 public class Body {
 
     //TODO: change modifiers.
-    public double mass;
-    public Vector3 massCenter; // position of the center of mass.
-    public Vector3 currentMovement;
+    private double mass;
+    private Vector3 massCenter; // position of the center of mass.
+    private Vector3 currentMovement;
+
 
     //TODO: define constructor.
+    public Body(double mass, Vector3 massCenter, Vector3 currentMovement) {
+        this.mass = mass;
+        this.massCenter = massCenter;
+        this.currentMovement = currentMovement;
+    }
 
     /**
      * Returns the distance between the centers of mass of this body and the specified body 'b'.
+     *
      * @param b the specified body, b != null.
      * @return the distance between the centers of mass of this body and the specified body 'b'.
      */
     public double distanceTo(Body b) {
-
         //TODO: implement method.
-        return 0;
+        return this.massCenter.distanceTo(b.massCenter);
     }
 
     /**
@@ -30,36 +36,42 @@ public class Body {
      * with the specified body 'b'. The returned vector is computed according to Newton's laws of
      * gravitation.
      * Hint: see method 'acceleration' in Simulation.java to find out how this is done.
+     *
      * @param b the specified body, b != null.
      * @return the acceleration vector.
      */
     public Vector3 acceleration(Body b) {
-
         //TODO: implement method.
-        return null;
+        Vector3 new_vek = b.massCenter.minus(this.massCenter);
+        double abstand = new_vek.length();
+        new_vek.normalize();
+        double a = Simulation.G * b.mass / (abstand * abstand);
+        return new_vek.times(a);
     }
 
     /**
      * Accelerates this body for one second according to the specified 'acceleration' vector
      * and updates the current movement accordingly.
      * Hint: see method 'accelerate' in Simulation.java to find out how this is done.
+     *
      * @param acceleration the acceleration vector, acceleration != null.
      */
     public void accelerate(Vector3 acceleration) {
-
         //TODO: implement method.
+        this.currentMovement = this.currentMovement.plus(acceleration);
+        this.massCenter = this.massCenter.plus(this.currentMovement);
     }
 
     /**
      * Returns the approximate radius of this body.
      * (It is assumed that the radius r is related to the mass m of the body by
      * r = Math.pow(m, 0.5), where m and r measured in solar mass units.)
+     *
      * @return the radius of this body.
      */
     public double getRadius() {
-
         //TODO: implement method.
-        return 0d;
+        return SpaceDraw.massToRadius(this.mass);
     }
 
     /**
@@ -68,13 +80,16 @@ public class Body {
      * returned body is given by the law of conservation of momentum. (The momentum of the
      * returned body is the sum of the momentums of 'this' and 'b').
      * Hint: see method 'merge' in Simulation.java to find out how this is done.
+     *
      * @param b the body with which 'this' is merged, b != null.
      * @return the body being formed by the collision.
      */
     public Body merge(Body b) {
-
         //TODO: implement method.
-        return null;
+        double new_mass = this.mass + b.mass;
+        Vector3 new_mass_center = this.massCenter.times(this.mass).plus(b.massCenter.times(b.mass)).times( 1.0 / new_mass);
+        Vector3 new_movement = this.currentMovement.times(this.mass).plus(b.currentMovement.times(b.mass)).times( 1.0 / new_mass);
+        return new Body(new_mass, new_mass_center, new_movement);
     }
 
     /**
@@ -83,23 +98,25 @@ public class Body {
      * (use a conversion of the real scale to the scale of the canvas as
      * in 'Simulation.java').
      * Hint: call the method 'drawAsFilledCircle' implemented in 'Vector3'.
+     *
      * @param cd the CodeDraw object used for drawing, cd != null.
      */
     public void draw(CodeDraw cd) {
-
         //TODO: implement method.
+        cd.setColor(SpaceDraw.massToColor(this.mass));
+        this.massCenter.drawAsFilledCircle(cd, SpaceDraw.massToRadius(this.mass));
     }
 
     /**
      * Returns a string with the information about this body including
      * mass, position (mass center) and current movement. Example:
      * "5.972E24 kg, position: [1.48E11, 0.0, 0.0] m, movement: [0.0, 29290.0, 0.0] m/s."
+     *
      * @return 'this' represented as a string.
      */
     public String toString() {
-
         //TODO: implement method.
-        return "";
+        return "\""+mass+" kg, position: "+massCenter.toString()+" m, movement: "+ currentMovement.toString()+" m/s.\"";
     }
 }
 
--- src/AB1/Vector3.java
+++ src/AB1/Vector3.java
@@ -8,64 +8,77 @@ import codedraw.CodeDraw;
 public class Vector3 {
 
     //TODO: change modifiers.
-    public double x;
-    public double y;
-    public double z;
+    private double x;
+    private double y;
+    private double z;
 
     //TODO: define constructor.
+    public Vector3(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 
     /**
      * Returns the sum of this vector and vector 'v'.
+     *
      * @param v another vector, v != null.
      * @return the sum of this vector and vector 'v'.
      */
     public Vector3 plus(Vector3 v) {
-
         //TODO: implement method.
-        return null;
+        double sum_x = this.x + v.x;
+        double sum_y = this.y + v.y;
+        double sum_z = this.z + v.z;
+        return new Vector3(sum_x, sum_y, sum_z);
     }
 
     /**
      * Returns the product of this vector and 'd'.
+     *
      * @param d the coefficient for the product.
      * @return the product of this vector and 'd'.
      */
     public Vector3 times(double d) {
-
         //TODO: implement method.
-        return null;
+        double mul_x = this.x * d;
+        double mul_y = this.y * d;
+        double mul_z = this.z * d;
+        return new Vector3(mul_x, mul_y, mul_z);
     }
 
     /**
      * Returns the sum of this vector and -1*v.
+     *
      * @param v another vector, v != null.
      * @return the sum of this vector and -1*v.
      */
     public Vector3 minus(Vector3 v) {
-
         //TODO: implement method.
-        return null;
+        Vector3 new_vektor = v.times(-1);
+        return this.plus(new_vektor);
     }
 
     /**
      * Returns the Euclidean distance of this vector to the specified vector 'v'.
+     *
      * @param v another vector, v != null.
      * @return the Euclidean distance of this vector to the specified vector 'v'.
      */
     public double distanceTo(Vector3 v) {
-
         //TODO: implement method.
-        return -1d;
+        Vector3 new_vektor = this.minus(v);
+        return Math.sqrt(new_vektor.x * new_vektor.x + new_vektor.y * new_vektor.y + new_vektor.z * new_vektor.z);
     }
 
     /**
      * Returns the norm of this vector.
+     *
      * @return the length (norm) of this vector.
      */
     public double length() {
-
         //TODO: implement method.
-        return 0;
+        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
     }
 
     /**
@@ -73,30 +86,39 @@ public class Vector3 {
      * The direction of the vector is not affected.
      */
     public void normalize() {
-
         //TODO: implement method.
+        double vektor_length = this.length();
+        if (vektor_length > 0) {
+            this.x /= vektor_length;
+            this.y /= vektor_length;
+            this.z /= vektor_length;
+        }
     }
 
     /**
      * Draws a filled circle with a specified radius centered at the (x,y) coordinates of this vector
      * in the canvas associated with 'cd'. The z-coordinate is not used.
-     * @param cd the CodeDraw object used for drawing.
+     *
+     * @param cd     the CodeDraw object used for drawing.
      * @param radius the radius > 0.
      */
     public void drawAsFilledCircle(CodeDraw cd, double radius) {
-
         //TODO: implement method.
+        double x = cd.getWidth() * (this.x + Simulation.SECTION_SIZE / 2) / Simulation.SECTION_SIZE;
+        double y = cd.getWidth() * (this.y + Simulation.SECTION_SIZE / 2) / Simulation.SECTION_SIZE;
+        radius = cd.getWidth() * radius / Simulation.SECTION_SIZE;
+        cd.fillCircle(x, y, Math.max(radius, 1.5));
     }
 
     /**
      * Returns the coordinates of this vector in brackets as a string
      * in the form "[x, y, z]", e.g., "[1.48E11, 0.0, 0.0]".
+     *
      * @return 'this' represented as a string.
      */
     public String toString() {
-
         //TODO: implement method.
-        return "";
+        return "[" + this.x + ", " + this.y + ", " + this.z + "]";
     }
 }
 
